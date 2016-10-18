c2a54635133298a38d192bfd1cb45b54fbbe6bf2
Daniel Habib <daniel.habib@dataprev.gov.br>
2016-10-18 09:33:32 -0200

    Fixing specs.

--- desktop/spec/clojure_karel/entities_spec.clj
+++ desktop/spec/clojure_karel/entities_spec.clj
@@ -2,41 +2,41 @@
   (:require [speclj.core :refer :all]
             [clojure-karel.entities :refer :all]))
 
-(describe "scenario1 karel moving"
+(describe "karel moving:"
   (before
-    (def entities [{:x 1 :y 1 :angle 0 :karel? true}
-                   {:x 2 :y 1 :chip? true}
-                   {:x 6 :y 2 :goal? true}
-                   {:x 4 :y 1 :wall? true}
-                   {:x 5 :y 1 :wall? true}
-                   {:x 6 :y 1 :wall? true}]))
+    (def entities [{:x 1 :y 1 :angle 0 :z 0 :karel? true :moving? true}
+                   {:x 2 :y 1 :z 0 :chip? true}
+                   {:x 6 :y 2 :z 0 :goal? true}
+                   {:x 4 :y 1 :z 0 :wall? true}
+                   {:x 5 :y 1 :z 0 :wall? true}
+                   {:x 6 :y 1 :z 0 :wall? true}]))
 
   (it "moves karel normally"
     (let [karel (first (move entities))]
-      (should= {:x 2 :y 1 :angle 0 :karel? true} karel)))
+      (should= {:x 2 :y 1  :z 0 :angle 0 :karel? true :moving? true} karel)))
 
   (it "doesnt move karel if there is a wall in the way"
     (let [karel (first (->> (move entities)
                             (move)
                             (move)))]
-      (should= {:x 3 :y 1 :angle 0 :karel? true} karel))))
+      (should= {:x 3 :y 1 :z 0 :angle 0 :karel? true :moving? true} karel))))
 
-(describe "scenario1 karel picking chips"
+(describe "picking chips:"
   (before
-    (def entities [{:x 1 :y 1 :angle 0 :karel? true}
-                   {:x 1 :y 1 :chip? true}
-                   {:x 6 :y 2 :goal? true}
-                   {:x 4 :y 1 :wall? true}
-                   {:x 5 :y 1 :wall? true}
-                   {:x 6 :y 1 :wall? true}]))
+    (def entities [{:x 1 :y 1 :z 0 :angle 0 :karel? true :moving? true}
+                   {:x 1 :y 1 :z 0 :chip? true}
+                   {:x 6 :y 2 :z 0 :goal? true}
+                   {:x 4 :y 1 :z 0 :wall? true}
+                   {:x 5 :y 1 :z 0 :wall? true}
+                   {:x 6 :y 1 :z 0 :wall? true}]))
 
   (it "does not get the chip"
     (let [chip (first (filter :chip? (pick (move entities))))]
-      (should= {:x 1 :y 1 :chip? true} chip)))
+      (should= {:x 1 :y 1 :z 0 :chip? true} chip)))
 
   (it "gets the chip"
     (let [chip (first (filter :chip? (pick entities)))]
-      (should= {:x 1 :y 1 :z 1 :chip? true} chip))))
+      (should= {:x 1 :y 1 :z 1 :chip? true :moving? true} chip))))
 
 
 (run-specs)
