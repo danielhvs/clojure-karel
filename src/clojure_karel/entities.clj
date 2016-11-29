eb4e53e681d78fe15ba2db050d15a96646c4ced8
danielhvs <danielhvs70@gmail.com>
2016-11-29 15:32:18 -0200

    cleaning unused code.

--- src/clojure_karel/entities.clj
+++ src/clojure_karel/entities.clj
@@ -172,43 +172,29 @@
             (next-state3 next-state)))))
 
 (defn solution1 [entities]
-  (let [
-        s1 (_right entities)
+  (let [s1 (_right entities)
         s2 (_grab (last s1))
         s3 (_right (last s2))
         s4 (_up (last s3))
         s5 (_right (last s4))
         s6 (_right (last s5))
         s7 (_right (last s6))
-        s8 (_drop (last s7))
-        ]
+        s8 (_drop (last s7))]
     [s1 s2 s3 s4 s5 s6 s7 s8]))
 
-
 (defn solution2-step [entities]
-  (let [
-        s1 (_up entities)
+  (let [s1 (_up entities)
         s2 (_grab (last s1))
         s3 (_down (last s2))
         s4 (_right (last s3))
         s5 (_down (last s4))
         s6 (_drop (last s5))
         s7 (_up (last s6))
-        s8 (_right (last s7))
-        result (-> s1 
-                   (into []) 
-                   (into s2)
-                   (into s3)
-                   (into s4)
-                   (into s5)
-                   (into s6)
-                   (into s7)
-                   (into s8))]
+        s8 (_right (last s7))]
     [s1 s2 s3 s4 s5 s6 s7 s8]))
 
 (defn solution2 [entities]
-  (let [
-        s1 (solution2-step entities)
+  (let [s1 (solution2-step entities)
         s2 (solution2-step (last (last s1)))
         s3 (solution2-step (last (last s2)))
         s4 (solution2-step (last (last s3)))
