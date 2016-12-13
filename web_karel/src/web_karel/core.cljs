f9d2e197055844793653e4b5ccac0abac4b6e5fb
danielhvs <danielhvs70@gmail.com>
2016-12-13 10:15:49 -0200

    Starting karel and reagent.

--- web_karel/src/web_karel/core.cljs
+++ web_karel/src/web_karel/core.cljs
@@ -1,24 +1,37 @@
 (ns web_karel.core
-  (:require [reagent.core :as reagent :refer [atom]]))
-  ;; (:require [karel.core :as k])
+  (:require [reagent.core :as reagent :refer [atom]]
+            [karel.karel :as k]))
 
 (enable-console-print!)
 
 (println "This text is printed from src/web_karel/core.cljs. Go ahead and edit it and see reloading in action.")
 
 ;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Karel the Robot learns Clojure"}))
+(defn karel-window []
+  [:center
+   [:h1
+    [:button
+     {:on-click
+      (fn level-1-click [e]
+        (swap! app-state assoc :text (str k/scenario1)))}
+     "Level 1"]]
+   [:h1
+    [:button
+     {:on-click
+      (fn level-2-click [e]
+        (swap! app-state assoc :text (str k/scenario2)))}
+     "Level 2"]]
+   [:h1 (:text @app-state)]
+   ])
 
-(defn hello-world []
-  [:h1 (:text @app-state)])
 
-(reagent/render-component [hello-world]
+(reagent/render-component [karel-window]
                           (. js/document (getElementById "app")))
 
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 (defn on-js-reload []
-  (swap! app-state assoc :text "new text")
-  (println "reloaded, man!"))
+  (println "reloaded!"))
   
