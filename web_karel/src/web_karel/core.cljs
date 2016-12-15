3351dd8067bf9e89212ae93ed2e986c0013f1c84
danielhvs <danielhvs70@gmail.com>
2016-12-15 08:46:24 -0200

    starting to draw.

--- web_karel/src/web_karel/core.cljs
+++ web_karel/src/web_karel/core.cljs
@@ -2,9 +2,36 @@
   (:require [reagent.core :as r :refer [atom]]
             [karel.core :as k]))
 
+(def board-size 3)
 (enable-console-print!)
 
 (println "This text is printed from src/web_karel/core.cljs. Go ahead and edit it and see reloading in action.")
+
+(defn blank [i j]
+  [:rect
+   {:width 0.9
+    :height 0.9
+    :fill "green"
+    :x (+ 0.05 i)
+    :y (+ 0.05 j)}])
+
+(defn circle [i j]
+  [:circle
+   {:r 0.35
+    :stroke "black"
+    :stroke-width 0.2
+    :fill "none"
+    :cx (+ 0.5 i)
+    :cy (+ 0.5 j)}])
+
+(defn cross [i j]
+  [:g {:stroke "blue"
+       :stroke-width 0.3
+       :transform
+       (str "translate(" (+ 0.5 i) "," (+ 0.5 j) ") "
+            "scale(0.3)")}
+   [:line {:x1 -1 :y1 -1 :x2 1 :y2 1}]
+   [:line {:x1 1 :y1 -1 :x2 -1 :y2 1}]])
 
 (defn timer-component []
   (let [n (r/atom 0)]
@@ -18,19 +45,17 @@
 (defonce app-state (atom {:text "Karel the Robot learns Clojure"}))
 (defn karel-window []
   [:center
-   [timer-component]
+   (into [:svg
+          {:view-box (str "0 0 " board-size " " board-size)
+           :width 500
+           :height 500}]
+         (for [i (range 2)] [cross i i]))
    [:h1
     [:button
      {:on-click
       (fn level-1-click [e]
         (swap! app-state assoc :text (str k/scenario1)))}
      "Level 1"]]
-   [:h1
-    [:button
-     {:on-click
-      (fn level-1-click [e]
-        (swap! app-state assoc :text (str (k/solution1 k/scenario1))))}
-     "Solution 1"]]
    [:h1
     [:button
      {:on-click
